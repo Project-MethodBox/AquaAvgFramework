11bea5313c51c6e1c939052d364c09f8d6755a45
Arabidopsis <arab@methodbox.top>
2024-09-01 11:52:13 +0800

    AquaFrameWork Dev_0.0.1
    1. 项目立项；
    2. 基本气泡开发开始。

---
+++ Bubble/CenterBubble.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace AquaAvgFramework.Bubble
+{
+    /// <summary>
+    /// CenterBubble.xaml 的交互逻辑
+    /// </summary>
+    public partial class CenterBubble : UserControl
+    {
+        public CenterBubble()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Background color of bubble card
+        /// </summary>
+        public SolidColorBrush BackGroundColor
+        {
+            get => (SolidColorBrush)GetValue(BackGroundColorProperty);
+            set => SetValue(BackGroundColorProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for BackGroundColor.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty BackGroundColorProperty =
+            DependencyProperty.Register(nameof(BackGroundColor), typeof(SolidColorBrush), typeof(CenterBubble), new PropertyMetadata(new SolidColorBrush(Color.FromRgb(11, 45, 14))));
+
+
+    }
+}
